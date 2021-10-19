dce9ddc4b372fbf2ea5aed77ac1c08c5880eb195
Sean <seanjmac@hotmail.com>
2021-10-18 18:39:45 -0700

    feat: add basic server boilerplate

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, NextFunction, Express } from 'express';
+import * as bodyParser from 'body-parser';
+
+// Instantiate the express server
+const app: Express = express();
+const port: number = 8000;
+
+// Ensure we have the proper parsing for requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Basic logging to check incoming requests
+app.use((req: Request, res: Response, next: NextFunction) => {
+	console.log("============= req.headers ===============");
+  console.log(req.headers);
+  console.log("============= req.body ===============");
+  console.log(req.body);
+  console.log("==============================");
+  next();
+});
+
+app.get('/', function(req: Request, res: Response) {
+	res.send({ hi: "there" })
+})
+
+app.listen(port, () => console.log(`App is listening on port ${port}`));
